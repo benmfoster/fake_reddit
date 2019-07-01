9dc0bc6aa99316fefe6db0af19e91030e42624d9
benmfoster <bfoster@marlboro.edu>
2019-07-01 10:58:46 -0400

    adds user downvoted post boolean hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
     validates :email, :name, presence: true, uniqueness: true
 
     def downvoted_post_ids
-        downvotes.map { |downvote| downvote.post.id }
+        is_downvoted = {}
+        downvotes.map do |downvote|
+            is_downvoted[downvote.post.id] = true
+        end
+        return is_downvoted
     end
 end
