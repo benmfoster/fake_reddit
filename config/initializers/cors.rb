97ae2551a1cbb7cf9ab2c03ede115d6014aa370f
benmfoster <bfoster@marlboro.edu>
2019-08-07 10:40:43 -0400

    adds localhost:3000 to allowed origins in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'lit-escarpment-59964.herokuapp.com'
+    origins 'localhost:8080', 'localhost:3000', 'lit-escarpment-59964.herokuapp.com'
 
     resource '*',
       headers: :any,
